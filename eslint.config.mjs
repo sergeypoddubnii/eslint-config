ffe95c8ea6d485360285618c3284fb371ed6c67b
serhiipiddubnii <poddubnii.sergey@gmail.com>
2024-08-01 22:10:08 +0300

    add plugins

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginAirbnb from 'eslint-config-airbnb';
+import pluginImport from 'eslint-plugin-import';
+
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...pluginImport.configs.recommended,
+    ...pluginAirbnb
+
+];
